086c5307543142e9af8ad72bcd1d6492fb4d87a4
alexchao26 <alexchaoo@gmail.com>
2020-12-29 16:07:50 -0500

    gcloud sdk, vscode settings

---
+++ .zsh/gcloud.zsh
@@ -0,0 +1,6 @@
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/usr/local/bin/google-cloud-sdk/path.zsh.inc' ]; then . '/usr/local/bin/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/usr/local/bin/google-cloud-sdk/completion.zsh.inc' ]; then . '/usr/local/bin/google-cloud-sdk/completion.zsh.inc'; fi
+
