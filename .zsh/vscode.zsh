93d6ef16a97d9905ba1c8ecd6eeb14045afbd74e
Alex Chao <alexchaoo@gmail.com>
2021-01-04 13:28:47 -0500

    new copies of vscode json files

--- .zsh/vscode.zsh
+++ .zsh/vscode.zsh
@@ -41,3 +41,12 @@ cpcode() {
   done
 }
 
+# copy and commit keybindings and settings files to .dotfiles repo
+vscode_dotfiles() {
+  cp ~/Library/Application\ Support/Code/User/keybindings.json ~/.vscode/copy.keybindings.json
+  cp ~/Library/Application\ Support/Code/User/settings.json ~/.vscode/copy.settings.json
+  
+  dotfiles add ~/.vscode/copy*
+  dotfiles commit -m 'new copies of vscode json files'
+  dotfiles push origin main
+}
