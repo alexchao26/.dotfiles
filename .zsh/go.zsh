78ef4a2310c9391d8b9b878911b8b0479a63c215
alexchao26 <alexchaoo@gmail.com>
2021-05-20 22:19:29 -0400

    update go test colorized sed

--- .zsh/go.zsh
+++ .zsh/go.zsh
@@ -15,4 +15,4 @@ alias gt="go test"
 alias go-test-v="go test -v . | sed ''/PASS/s//\$(printf \"\\033[32mPASS\\033[0m\")/'' | sed ''/FAIL/s//\$(printf \"\033[31mFAIL\\033[0m\")/'' | sed ''/RUN/s//\$(printf \"\033[4;37mRUN\\033[0m\")/''"
 
 # pipe an output into this alias, i.e. make test | colorize
-alias colorize="sed ''/PASS/s//\$(printf \"\\033[32mPASS\\033[0m\")/'' | sed ''/FAIL/s//\$(printf \"\033[31mFAIL\\033[0m\")/'' | sed ''/RUN/s//\$(printf \"\033[4;37mRUN\\033[0m\")/''"
+alias colorize="sed ''/PASS/s//\$(printf \"\\033[32mPASS\\033[0m\")/'' | sed ''/FAIL/s//\$(printf \"\033[31mFAIL\\033[0m\")/''"
