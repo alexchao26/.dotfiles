17fd051299d58e1acc1a0c57637c61c211b202e7
Alex Chao <alexchaoo@gmail.com>
2021-07-22 12:31:38 -0400

    update colorize go test alias

--- .zsh/go.zsh
+++ .zsh/go.zsh
@@ -12,7 +12,7 @@ alias gt="go test"
 
 # colorize the go test output
 # escape $, " (within double quotes) and \ (escape itself)
-alias go-test-v="go test -v . | sed ''/PASS/s//\$(printf \"\\033[32mPASS\\033[0m\")/'' | sed ''/FAIL/s//\$(printf \"\033[31mFAIL\\033[0m\")/'' | sed ''/RUN/s//\$(printf \"\033[4;37mRUN\\033[0m\")/''"
+alias go-test-v="go test -v . | sed ''/PASS/s//\$(printf \"\\033[32mPASS\\033[0m\")/'' | sed ''/FAIL/s//\$(printf \"\033[31mFAIL\\033[0m\")/''"
 
 # pipe an output into this alias, i.e. make test | colorize
-alias colorize="sed ''/PASS/s//\$(printf \"\\033[32mPASS\\033[0m\")/'' | sed ''/FAIL/s//\$(printf \"\033[31mFAIL\\033[0m\")/'' | sed ''/RUN/s//\$(printf \"\033[4;37mRUN\\033[0m\")/''"
+alias colorize="sed ''/PASS/s//\$(printf \"\\033[32mPASS\\033[0m\")/'' | sed ''/FAIL/s//\$(printf \"\033[31mFAIL\\033[0m\")/''"
